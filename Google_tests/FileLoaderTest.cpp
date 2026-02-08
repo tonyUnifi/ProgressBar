29ed370f1d3f8d7ed5138fe1be1c52e5e429dac0
tony <costache.tony@gmail.com>
2026-02-08 13:03:27 +0100

    Added Pattern Observer test

--- Google_tests/FileLoaderTest.cpp
+++ Google_tests/FileLoaderTest.cpp
@@ -4,6 +4,27 @@
 
 #include "gtest/gtest.h"
 #include "../FileLoader.h"
+#include "../Observer.h"
+
+class MockProgressBar : public Observer {
+public:
+    explicit MockProgressBar(int totalSteps): value(0), maximum(totalSteps) {}
+
+    void update() override {
+        value++;
+    }
+
+    int getValue() const {
+        return value;
+    }
+
+    int getMaximum() const {
+        return maximum;
+    }
+private:
+    int value;
+    int maximum;
+};
 
 class FileLoaderTest : public ::testing::Test {
 protected:
@@ -13,15 +34,19 @@ protected:
         myFile << "This file has 27 characters";
         myFile.close();
         fileLoader = new FileLoader(testFileName);
+        progressBar = new MockProgressBar(fileLoader -> getTotalSteps());
     }
     void TearDown() override {
         delete fileLoader;
         std::remove(testFileName.c_str());
+        delete progressBar;
     }
     std::string testFileName;
     FileLoader* fileLoader;
+    MockProgressBar* progressBar;
 };
 
+//FileLoader test
 TEST_F(FileLoaderTest, correctInit) {
     ASSERT_EQ(0, fileLoader->getCurrentSteps());
 }
@@ -38,4 +63,18 @@ TEST_F(FileLoaderTest, correctFileSize) {
 TEST_F(FileLoaderTest, correctLoading) {
     fileLoader -> loadFile();
     ASSERT_EQ("This file has 27 characters", fileLoader->getFileContent());
+}
+
+//Pattern observer test
+TEST_F(FileLoaderTest, correctUpdateAfterSubscribe) {
+    fileLoader -> subscribe(progressBar);
+    fileLoader -> loadFile();
+    ASSERT_EQ(progressBar -> getValue(), fileLoader->getTotalSteps());
+}
+
+TEST_F(FileLoaderTest, correctUnsubscribe) {
+    fileLoader -> subscribe(progressBar);
+    fileLoader -> unsubscribe(progressBar);
+    fileLoader -> loadFile();
+    ASSERT_EQ(progressBar -> getValue(), 0);
 }
